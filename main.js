e988d4dc2125dc8905150f94740c26405073892c
KauanyMedardo <149601111+KauanyMedardo@users.noreply.github.com>
2024-04-11 16:44:43 -0300

    h

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ const dataAtual = new Date ();
 let dataNiver = prompt ("Data do próximo viver:"); 
 dataNiver = new Date ( dataNiver+"T 23:59:59"); 
 let diasQueFaltam = Math.foor ((dataNiver-dataAtual)/86400000);  
+document.querSelector ("#dias_restantes").textContet=diasQueFaltam;
